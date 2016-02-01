3035129197ed24a99b9d40b92cd1feb213b469f6
cretin45 <cretin45@gmail.com>
2016-02-01 15:16:05 -0800

    SetupWizard: Ensure UI doesn't show when user is guest
    Issue-id: CYNGNOS-1736
    
    Change-Id: Ibcd85ca3f90a6ab35a3ae6da622f59786ddcbcd3

--- src/com/xperience/setupwizard/ui/SetupWizardActivity.java
+++ src/com/xperience/setupwizard/ui/SetupWizardActivity.java
@@ -83,6 +83,10 @@ public class SetupWizardActivity extends Activity implements SetupDataCallbacks 
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final boolean isOwner = SetupWizardUtils.isOwner();
+        if (!isOwner) {
+            finish();
+        }
         final View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(UI_FLAGS);
         decorView.setOnSystemUiVisibilityChangeListener(
@@ -156,6 +160,9 @@ public class SetupWizardActivity extends Activity implements SetupDataCallbacks 
         final View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(UI_FLAGS);
         super.onResume();
+        if (isFinishing()) {
+            return;
+        }
         if (mSetupData.isFinished()) {
             mHandler.postDelayed(new Runnable() {
                 @Override
@@ -173,15 +180,19 @@ public class SetupWizardActivity extends Activity implements SetupDataCallbacks 
     @Override
     protected void onPause() {
         super.onPause();
-        mSetupData.onPause();
+        if (mSetupData != null) {
+            mSetupData.onPause();
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mSetupData.onDestroy();
-        mSetupData.unregisterListener(this);
-        unregisterReceiver(mSetupData);
+        if (mSetupData != null) {
+            mSetupData.onDestroy();
+            mSetupData.unregisterListener(this);
+            unregisterReceiver(mSetupData);
+        }
     }
 
     @Override
