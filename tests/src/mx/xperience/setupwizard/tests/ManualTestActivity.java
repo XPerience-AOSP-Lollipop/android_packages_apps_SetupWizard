d5d44acc16586d85c52a656be4bf88ba60e44343
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-02-19 02:51:17 -0600

    fix test
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- tests/src/mx/xperience/setupwizard/tests/ManualTestActivity.java
+++ tests/src/mx/xperience/setupwizard/tests/ManualTestActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.setupwizard.tests;
+package mx.xperience.setupwizard.tests;
 
 
 import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DEFAULT;
@@ -73,7 +73,7 @@ public class ManualTestActivity extends Activity {
             Settings.Secure.putInt(getContentResolver(), Settings.Secure.USER_SETUP_COMPLETE, 0);
             final Intent intent = new Intent("android.intent.action.MAIN");
             intent.addCategory("android.intent.category.HOME");
-            resetComponentSets("com.cyanogenmod.setupwizard", GET_ACTIVITIES |
+            resetComponentSets("mx.xperience.setupwizard", GET_ACTIVITIES |
                     GET_RECEIVERS | GET_SERVICES | MATCH_DISABLED_COMPONENTS);
             resetComponentSets("com.google.android.setupwizard", GET_ACTIVITIES |
                     GET_RECEIVERS | GET_SERVICES | MATCH_DISABLED_COMPONENTS);
@@ -109,7 +109,7 @@ public class ManualTestActivity extends Activity {
         Settings.Secure.putInt(getContentResolver(), Settings.Secure.USER_SETUP_COMPLETE, 1);
         Intent intent = new Intent("android.intent.action.MAIN");
         intent.addCategory("android.intent.category.HOME");
-        disableComponentSets("com.cyanogenmod.setupwizard",  GET_ACTIVITIES |
+        disableComponentSets("mx.xperience.setupwizard",  GET_ACTIVITIES |
                 GET_RECEIVERS | GET_SERVICES);
         try {
             disableComponentSets("com.google.android.setupwizard",  GET_ACTIVITIES |
